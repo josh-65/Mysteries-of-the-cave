fa9a57bd924853ed69a525670c7c733375ab3193
Josh-65 <joshuauk2004@gmail.com>
2020-09-23 20:34:32 +0100

    Update timedate.js

--- scripts/timedate.js
+++ scripts/timedate.js
@@ -1,7 +1,9 @@
 function timedate() {
     var today = new Date();
     var date = today.getDay()+'/'+(today.getMonth()+1)+'/'+today.getFullYear();
-    var time = today.getSeconds() + ":" + today.getMinutes() + ":" + today.getHours();
+    var hours = today.getHours() % 12 || 12;
+    var time = hours + ":" + today.getMinutes() + ":" + today.getSeconds();
+
 
     var output = time+' - '+date;
     document.getElementById("timedate").innerHTML = output;
