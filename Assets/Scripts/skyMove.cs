b3ad4e1d3e92b3f1ee3103ff8068ac881b47830e
Josh-65 <joshuauk2004@gmail.com>
2020-10-03 16:19:16 +0100

    1

---
+++ Assets/Scripts/skyMove.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine; 
+using System.Collections;
+
+public class skyMove : MonoBehaviour
+{
+    float target;
+	void Start() 
+	{
+
+	}
+
+	void Update()
+	{
+        target += Time.deltaTime / 125;
+
+        transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x, transform.position.y, target), 0.05f);
+
+	}
+}
