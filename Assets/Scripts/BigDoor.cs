1a9e06f196d5db0839ea096ff035052e20803a09
Josh-65 <joshuauk2004@gmail.com>
2020-10-04 22:51:49 +0100

    Started space station

---
+++ Assets/Scripts/BigDoor.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BigDoor : MonoBehaviour {
+
+	void Update(){
+			//Animation.Play("Big Door - left");
+	}
+}
