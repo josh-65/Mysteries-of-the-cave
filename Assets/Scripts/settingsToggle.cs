d0cbc141520faf1490d207e7766d05a0146eda2d
Josh-65 <joshuauk2004@gmail.com>
2020-10-06 10:32:11 +0100

    Started settings menu

---
+++ Assets/Scripts/settingsToggle.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class settingsToggle : MonoBehaviour
+{
+    public string canvas1;
+    public string canvas2;
+
+    public void Menutoggle() {
+
+    }
+}
