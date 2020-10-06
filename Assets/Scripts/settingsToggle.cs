a1798ea97960fb10b328a94e6e1bd591aed7db6a
Josh-65 <joshuauk2004@gmail.com>
2020-10-06 18:10:41 +0100

    settings update

--- Assets/Scripts/settingsToggle.cs
+++ Assets/Scripts/settingsToggle.cs
@@ -4,10 +4,26 @@ using UnityEngine;
 
 public class settingsToggle : MonoBehaviour
 {
-    public string canvas1;
-    public string canvas2;
+     private Canvas CanvasObject; 
+     private Canvas CanvasObject1;
+ 
+     void Start () 
+     {
+         CanvasObject = GameObject.Find("Main Menu").GetComponent<Canvas>();
+         CanvasObject1 = GameObject.Find("Settings Menu").GetComponent<Canvas>();
 
-    public void Menutoggle() {
-
-    }
+         CanvasObject.GetComponent<Canvas> ().enabled = true;
+         CanvasObject1.GetComponent<Canvas> ().enabled = false;
+     }
+ 
+     public void ToggleCanvas ()
+     {
+         if (CanvasObject.enabled == true) {
+             CanvasObject.GetComponent<Canvas> ().enabled = false;
+             CanvasObject1.GetComponent<Canvas> ().enabled = true;
+         } else {
+             CanvasObject.GetComponent<Canvas> ().enabled = true;
+             CanvasObject1.GetComponent<Canvas> ().enabled = false;
+         } 
+     }
 }
